5d31a0e3ef24dcc5aee6311ba8f394ad772dba9f
TheManStitch <94526792+TheManStitch@users.noreply.github.com>
2022-05-17 13:30:53 +0200

    kleine aanpassingen

--- src/main/java/com/proj2/Admin.java
+++ src/main/java/com/proj2/Admin.java
@@ -2,7 +2,9 @@ package com.proj2;
 
 public class Admin extends Person
 {
-
+    public Admin(String name, String email, String password, String residence, int points) {
+        super(name, email, password, residence, points);
+    }
 
     public void addUser() {
 
