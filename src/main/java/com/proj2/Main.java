4730fd598fab0594bbedc62e4a9ea463115a6a7f
Petzr <peterjansen812@gmail.com>
2022-05-17 13:29:56 +0200

    Created 2 constructurs. 1 voor geven van een HahMap. 1 voor het maken van Standaard hashMap

---
+++ src/main/java/com/proj2/Main.java
@@ -0,0 +1,6 @@
+package com.proj2;
+
+public class Main {
+
+    Vehicle vehicle = new Vehicle();
+}
--- src/main/java/com/proj2/Vehicle.java
+++ src/main/java/com/proj2/Vehicle.java
@@ -4,9 +4,32 @@ import java.util.HashMap;
 
 public class Vehicle
 {
-    private HashMap<String, Integer> vehicles;
+    private HashMap<String, Double> vehicles = new HashMap<>();
 
-    public int calculatePoints() {
+    public Vehicle(HashMap<String, Double> vehicles) {
+        this.vehicles = vehicles;
+    }
+    public Vehicle() {
+        vehicles.put("auto", 1.0);
+        vehicles.put("vliegtuig", 3.0);
+        vehicles.put("helikopter", 3.0);
+        vehicles.put("bus", 0.8);
+        vehicles.put("fiets", 0.0);
+        vehicles.put("tram", 0.3);
+        vehicles.put("trein", 0.3);
 
+    }
+
+    public void addVehicle(String vehicleName, Double pointsOfVehicle) {
+        vehicles.put(vehicleName, pointsOfVehicle);
+    }
+
+    public double calculatePoints() {
+        return 5.0;
+
+    }
+
+    public double getPointsOfVehicle(String vehicleName) {
+        return vehicles.get(vehicleName);
     }
 }
