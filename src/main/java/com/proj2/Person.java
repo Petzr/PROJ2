14b799560f1853976693575a511a713fefa049eb
Petzr <peterjansen812@gmail.com>
2022-05-19 15:01:37 +0200

    debug

--- src/main/java/com/proj2/Person.java
+++ src/main/java/com/proj2/Person.java
@@ -6,14 +6,12 @@ public abstract class Person
     private String email;
     private String password;
     private String residence;
-    private int points;
 
-    public Person(String name, String email, String password, String residence, int points) {
+    public Person(String name, String email, String password, String residence) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.residence = residence;
-        this.points = points;
     }
 }
 
