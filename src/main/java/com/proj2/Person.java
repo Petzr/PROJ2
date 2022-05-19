0f8c54f2cd198bc3acc017e0e37750322e71e80d
Petzr <peterjansen812@gmail.com>
2022-05-19 15:03:52 +0200

    added getters and setters voor makkelijk gebruik

--- src/main/java/com/proj2/Person.java
+++ src/main/java/com/proj2/Person.java
@@ -6,15 +6,46 @@ public abstract class Person
     private String email;
     private String password;
     private String residence;
-    private int points;
 
-    public Person(String name, String email, String password, String residence, int points) {
+    public Person(String name, String email, String password, String residence) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.residence = residence;
-        this.points = points;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
 }
 
 
