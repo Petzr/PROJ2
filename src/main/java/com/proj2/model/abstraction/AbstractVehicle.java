bf107d50583401ab75bf578f1e74f01cce0caa44
Petzr <peterjansen812@gmail.com>
2022-05-23 16:18:00 +0200

    created simpel vehicle

---
+++ src/main/java/com/proj2/model/abstraction/AbstractVehicle.java
@@ -0,0 +1,16 @@
+package com.proj2.model.abstraction;
+
+public class AbstractVehicle extends AbstractEntity{
+
+    private String name;
+    private double modifier;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getModifier() {
+        return modifier;
+    }
+
+}
