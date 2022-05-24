3edc336f8c4c83749d32d6e122fec2883e4d2a58
Petzr <peterjansen812@gmail.com>
2022-05-24 14:40:34 +0200

    abstract class gemaakt van Vehicle

--- src/main/java/com/proj2/model/abstraction/AbstractVehicle.java
+++ src/main/java/com/proj2/model/abstraction/AbstractVehicle.java
@@ -1,21 +1,10 @@
 package com.proj2.model.abstraction;
 
-public class AbstractVehicle extends AbstractEntity{
+public abstract class AbstractVehicle extends AbstractEntity{
 
-    private String name;
-    private double modifier;
-
-    public AbstractVehicle(String name, double modifier) {
-        this.name = name;
-        this.modifier = modifier;
+    public double calculatePoint() {
+        return getModifier();
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getModifier() {
-        return modifier;
-    }
+    public abstract double getModifier();
 
 }
