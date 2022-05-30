4845a875b03e33152b41bd8cacd581668773bdf4
Luca Don <20096550@student.hhs.nl>
2022-05-30 10:41:04 +0200

    First commit

--- src/main/java/com/proj2/model/person/Admin.java
+++ src/main/java/com/proj2/model/person/Admin.java
@@ -3,6 +3,11 @@ package com.proj2.model.person;
 import com.proj2.model.abstraction.AbstractPerson;
 
 public class Admin extends AbstractPerson {
+
+    public Admin(String name, String email, String password) {
+        super(name, email, password);
+    }
+
     public void addUser(){}
     public void removeUser(){}
 }
--- src/main/java/com/proj2/model/person/User.java
+++ src/main/java/com/proj2/model/person/User.java
@@ -5,6 +5,14 @@ import com.proj2.model.abstraction.AbstractPerson;
 public class User extends AbstractPerson {
 
     private int points;
-    public void addPoints(){}
+
+    public User(String name, String email, String password) {
+        super(name, email, password);
+        this.points = 0;
+    }
+
+    public void addPoints(int points){
+        this.points += points;
+    }
 
 }
