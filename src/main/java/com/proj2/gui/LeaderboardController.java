54493057c1d9ba478c76f795328617b67f7f90c7
Petzr <peterjansen812@gmail.com>
2022-05-31 21:43:40 +0200

    added all userinfo and interfaces

--- src/main/java/com/proj2/gui/LeaderboardController.java
+++ src/main/java/com/proj2/gui/LeaderboardController.java
@@ -1,8 +1,25 @@
 package com.proj2.gui;
 
+import com.proj2.model.abstraction.AbstractPerson;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 
-public class LeaderboardController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LeaderboardController implements Initializable, IControllerInfo
 {
+    private AbstractPerson user;
+
     public TableView leaderboardTable;
+
+    @Override
+    public void setUser(AbstractPerson user) {
+        this.user = user;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/com/proj2/gui/LogInController.java
+++ src/main/java/com/proj2/gui/LogInController.java
@@ -1,11 +1,16 @@
 package com.proj2.gui;
 
+import com.proj2.model.abstraction.AbstractPerson;
 import com.proj2.model.person.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LogInController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInController implements Initializable, IControllerInfo
 {
     public TextField emailField;
     public PasswordField passwordField;
@@ -13,5 +18,13 @@ public class LogInController
     public void logInButtonClick(ActionEvent actionEvent)
     {
         new UserDashboard(new User("Peter", "email", "password"));
+    }
+
+    @Override
+    public void setUser(AbstractPerson user) {}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
