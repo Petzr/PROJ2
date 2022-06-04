1ebf4593e67ce9fb448a9c7e7159debc80384b51
b <bilal_mass@live.nl>
2022-06-04 17:10:46 +0200

    Registratie pagina, methods moeten nog worden gedaan

---
+++ src/main/java/com/proj2/gui/RegistrationController.java
@@ -0,0 +1,36 @@
+package com.proj2.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+public class RegistrationController  {
+
+    @FXML
+    private TextField nametf;
+    @FXML
+    private TextField mailtf;
+    @FXML
+    private PasswordField passwordtf;
+    @FXML
+    private PasswordField passwordtf2;
+    @FXML
+    private Button registerbttn;
+    @FXML
+    private Button returnbttn;
+
+    public void retbttn(MouseEvent mouseEvent) throws IOException {
+        // return naar de vorige pagina
+    }
+
+    public void regbttn(MouseEvent mouseEvent) throws IOException {
+        if(passwordtf.getText().equals(passwordtf2.getText())) {
+            //new user..
+        }
+    }
+}
