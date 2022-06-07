31819e612246ea5c591d0fa3bcc8b7f41370fb34
daner <daner.elias@gmail.com>
2022-06-07 14:18:56 +0200

    Andere admin schermen gemaakt

---
+++ src/main/java/com/proj2/gui/AdminDashboardController.java
@@ -0,0 +1,61 @@
+package com.proj2.gui;
+
+import com.proj2.model.abstraction.AbstractPerson;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class AdminDashboardController
+{
+    private AbstractPerson user;
+
+    @FXML
+    private Label nameUser;
+
+    @FXML
+    public void logOutButton(ActionEvent actionEvent)
+    {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void nieuweGebruiker(ActionEvent event) {
+        // dit is nodig om de stage te bepalen
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        // hier wordt de nieuwe scene gemaakt en de user meegegeven
+        Scene scene = IControllerInfo.createNewScene(user, "/com/proj2/registration.fxml", new deletingUserController());
+
+        // spreekt voorzich denk...
+        if (scene != null) {
+            stage.setScene(scene);
+        }
+    }
+
+    @FXML
+    void verwijderGebruiker(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showLeaderboard(ActionEvent event) {
+        // dit is nodig om de stage te bepalen
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        // hier wordt de nieuwe scene gemaakt en de user meegegeven
+        Scene scene = IControllerInfo.createNewScene(user, "/com/proj2/leaderboard.fxml", new LeaderboardController());
+
+        // spreekt voorzich denk...
+        if (scene != null) {
+            stage.setScene(scene);
+        }
+    }
+
+}
---
+++ src/main/java/com/proj2/gui/Registration.java
@@ -0,0 +1,33 @@
+package com.proj2.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Registration
+{
+
+    @FXML
+    private TextField mailtf;
+
+    @FXML
+    private TextField nametf;
+
+    @FXML
+    private PasswordField passwordtf;
+
+    @FXML
+    private PasswordField passwordtf2;
+
+    @FXML
+    void backToDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerUser(ActionEvent event) {
+
+    }
+
+}
