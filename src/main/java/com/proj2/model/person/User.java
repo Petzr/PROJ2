c6307c4c368ef56cedd6c4aff3599d3cd2cffc6f
Petzr <peterjansen812@gmail.com>
2022-06-13 11:42:20 +0200

    werkende update voor de leaderboard. Elke aanpassing met punten wordt geupdate.

--- src/main/java/com/proj2/model/person/User.java
+++ src/main/java/com/proj2/model/person/User.java
@@ -3,6 +3,7 @@ package com.proj2.model.person;
 import com.proj2.model.Travel;
 import com.proj2.model.abstraction.AbstractPerson;
 import com.proj2.model.vehicles.AbstractVehicle;
+import com.proj2.service.Logic;
 
 import java.util.ArrayList;
 
@@ -30,12 +31,7 @@ public class User extends AbstractPerson {
         Travel travel = new Travel(vehicle, aantalKM);
         points += travel.getPoints();
         travels.add(travel);
-        updateObservers();
-    }
-
-    private void updateObservers() {
-        setChanged();
-        notifyObservers();
+        Logic.get_organization().updateObservers();
     }
 
     public ArrayList<Travel> getTravels()
