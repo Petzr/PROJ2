c6f24f7cb3a33d48e83e49bbde3e7607db0bbf02
Luca Don <20096550@student.hhs.nl>
2022-06-15 17:18:33 +0200

    fixed deleteReward

--- src/main/java/com/proj2/gui/AdminRewardController.java
+++ src/main/java/com/proj2/gui/AdminRewardController.java
@@ -3,6 +3,10 @@ package com.proj2.gui;
 import com.proj2.model.Reward;
 import com.proj2.model.abstraction.AbstractPerson;
 import com.proj2.model.person.Admin;
+import com.proj2.model.person.User;
+import com.proj2.service.Logic;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,12 +15,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
-public class AdminRewardController implements Initializable, IControllerInfo
+public class AdminRewardController implements Initializable, IControllerInfo, Observer
 {
 
     private AbstractPerson user;
@@ -48,8 +55,12 @@ public class AdminRewardController implements Initializable, IControllerInfo
     }
 
     @FXML
-    void deleteReward(ActionEvent event) {
-
+    void deleteReward(ActionEvent actionEvent) {
+        Reward reward = rewardsTable.getSelectionModel().getSelectedItem();
+        if (reward != null) {
+            ((Admin) user).removeReward(reward);
+            backToDashboard(actionEvent);
+        } //else errorMessage.setText("Please select a reward before collecting it.");
     }
 
     @Override
@@ -61,6 +72,27 @@ public class AdminRewardController implements Initializable, IControllerInfo
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        Logic.get_organization().addObserver(this);
+        createTable();
+    }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("update leaderboard " + user.getName());
+        rewardsTable.refresh();
+    }
+
+    private void createTable(){
+        colomnItems.setCellValueFactory(new PropertyValueFactory<Reward, String>("name"));
+        colomnCost.setCellValueFactory(new PropertyValueFactory<Reward, Integer>("points"));
+        colomnCost.setSortType(TableColumn.SortType.DESCENDING);
+
+        rewardsTable.setItems(getRewardList());
+    }
+
+    private ObservableList<Reward> getRewardList() {
+        ObservableList<Reward> list = FXCollections.observableArrayList();
+        list.addAll(Logic.get_organization().getRewards());
+        return list;
     }
 }
