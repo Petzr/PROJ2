8f6efde70201e14af02fea30a06e952f056936bc
daner <daner.elias@gmail.com>
2022-06-16 14:04:48 +0200

    Meer classes verwijdert

--- src/main/java/com/proj2/service/AuthenticationProvider.java
+++
@@ -1,7 +0,0 @@
-package com.proj2.service;
-
-import com.proj2.model.UserRepository;
-
-public class AuthenticationProvider {
-    private final UserRepository userRepository = UserRepository.getInstance();
-}
