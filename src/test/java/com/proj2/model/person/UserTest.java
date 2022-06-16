a9a2ac66407b78b34fb94ae9ae048011b4a1e25e
daner <daner.elias@gmail.com>
2022-06-16 15:21:09 +0200

    Tests User

---
+++ src/test/java/com/proj2/model/person/UserTest.java
@@ -0,0 +1,24 @@
+package com.proj2.model.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest
+{
+
+    @Test
+    void getPoints()
+    {
+    }
+
+    @Test
+    void newTravel()
+    {
+    }
+
+    @Test
+    void removeReward()
+    {
+    }
+}
